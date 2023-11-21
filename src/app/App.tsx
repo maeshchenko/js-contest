7a50e91be36726056367c55de8695d326f390926
Mikhail Eshchenko <mikhail.eshchenko@gmail.com>
2023-11-21 20:28:35 +0700

    add scss support

---
+++ src/app/App.tsx
@@ -0,0 +1,5 @@
+import './styles/index.scss';
+
+export const App = () => {
+   return <h1 className={'header'}>Hello world from App!</h1>
+}
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
+import 'webpack-dev-server';
 
 export type TBuildMode = 'production' | 'development';
 
@@ -25,7 +26,19 @@ export default (env:IBuildEnv) => {
                     use: 'ts-loader',
                     exclude: /node_modules/,
                 },
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        // Creates `style` nodes from JS strings
+                        "style-loader",
+                        // Translates CSS into CommonJS
+                        "css-loader",
+                        // Compiles Sass to CSS
+                        "sass-loader",
+                    ],
+                },
             ],
+
         },
         resolve: {
             extensions: ['.tsx', '.ts', '.js'],
