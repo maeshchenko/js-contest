a0da4daf61982a33a6c8d760d28db664d65b4713
Mikhail Eshchenko <mikhail.eshchenko@gmail.com>
2023-11-21 21:03:26 +0700

    add devserver and css build rules

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,9 @@
 import path from 'path';
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
-import 'webpack-dev-server';
+import {Configuration} from 'webpack-dev-server';
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import TerserPlugin from "terser-webpack-plugin";
 
 export type TBuildMode = 'production' | 'development';
 
@@ -10,14 +12,23 @@ export interface IBuildEnv {
     port: number;
 }
 
-export default (env:IBuildEnv) => {
+export default (env:IBuildEnv): webpack.Configuration => {
+    const isDev = env.mode === 'development';
+    const PORT = env.port ?? 3000;
     const config = {
         mode: env.mode ?? 'development',
         entry: path.resolve(__dirname, 'src', 'index.tsx'),
-        devtool: 'inline-source-map',
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: !isDev ? undefined : {
+            port: PORT,
+            open: true,
+            hot: true,
+            historyApiFallback: true
+        } as Configuration,
         output: {
-            filename: 'main.js',
+            filename: 'bundle.[contenthash].js',
             path: path.resolve(__dirname, 'dist'),
+            clean: true
         },
         module: {
             rules: [
@@ -30,9 +41,9 @@ export default (env:IBuildEnv) => {
                     test: /\.s[ac]ss$/i,
                     use: [
                         // Creates `style` nodes from JS strings
-                        "style-loader",
+                        isDev ? "style-loader" : MiniCssExtractPlugin.loader,
                         // Translates CSS into CommonJS
-                        "css-loader",
+                        'css-loader',
                         // Compiles Sass to CSS
                         "sass-loader",
                     ],
@@ -42,13 +53,30 @@ export default (env:IBuildEnv) => {
         },
         resolve: {
             extensions: ['.tsx', '.ts', '.js'],
+            preferAbsolute: true,
+            modules: [path.resolve(__dirname, 'src'), "node_modules"],
+            mainFiles: ["index"],
+            alias: {},
         },
         plugins: [
             new HtmlWebpackPlugin({
                 template: path.resolve(__dirname, 'public', 'index.html')
             }),
-            new webpack.ProgressPlugin()
+            new webpack.ProgressPlugin(),
+            new MiniCssExtractPlugin({
+                filename: 'css/[name].[contenthash].css',
+                chunkFilename: 'css/[id].[contenthash].css',
+            })
         ],
+        optimization: {
+            minimizer: [
+                new TerserPlugin(
+                    {
+                        extractComments: false
+                    }
+                )
+            ]
+        }
     }
 
     return config;
